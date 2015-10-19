6474c59f0c7640b8805e177264b68e8c5d782cd8
vsoch <vsochat@stanford.edu>
2015-10-19 16:19:48 -0700

    first test of site

--- script/make_site.py
+++ script/make_site.py
@@ -2,6 +2,7 @@ from psiturkpy.experiment import get_experiments, get_validation_fields
 from psiturkpy.battery import template_experiments, get_timing_js, get_load_js, get_concat_js
 from psiturkpy.vm import custom_battery_download
 from psiturkpy.utils import sub_template
+from random import choice
 from glob import glob
 import shutil
 import os
@@ -15,35 +16,52 @@ experiments = get_experiments("%s/experiments" %tmpdir,load=True)
 
 # We will write an index, and a load_experiment.js file
 output_index = os.path.abspath("../index.html")
-output_js = os.path.abspath("../static/js/load_experiments.js")
-output_hub = os.path.abspath("../exp_hub.html")
+output_table = os.path.abspath("../table.html")
 data_folder = os.path.abspath("../data")
 
 # Let's make a dataframe of valid experiments
-fields = ["folder","preview"] + [f[0] for f in get_validation_fields()]
+# fields = ["folder","preview"] + [f[0] for f in get_validation_fields()]
+fields = ['preview','tag',
+          'contributors','time','notes',
+          'cognitive_atlas_concept_id',
+          'cognitive_atlas_concept',
+          'cognitive_atlas_task_id']
+
 valid = pandas.DataFrame(columns=fields)
 
 # Make a table of experiment information
 for experiment in experiments:
     for field in experiment[0].keys():
-        values = experiment[0][field]
-        # Join lists with a comma
-        if field == "reference":
-            if values != '':
-                values = '<a href="%s" target="_blank">%s</a>' %(values,values)
-        if isinstance(values,list):
-            values = ",".join(values)
-        valid.loc[experiment[0]["tag"],field] = values
+        if field in fields:
+            values = experiment[0][field]
+            # Join lists with a comma
+            if field == "reference":
+                if values != '':
+                    values = '<a href="%s" target="_blank">%s</a>' %(values,values)
+            if isinstance(values,list):
+                values = ",".join(values)
+            valid.loc[experiment[0]["tag"],field] = values
 
     # Add a preview link
-    valid.loc[experiment[0]["tag"],"preview"] = '<a href="exp_hub?exp=%s" target="_blank">DEMO</a>' %(experiment[0]["tag"])
-    valid.loc[experiment[0]["tag"],"folder"] = experiment[0]["tag"]
+    valid.loc[experiment[0]["tag"],"preview"] = '<a href="%s.html" target="_blank">DEMO</a>' %(experiment[0]["tag"])
 
+
+# For each experiment, we will prepare an interactive node for the site
+nodes = []
+for experiment in experiments:
+    nodes.append('{"cluster": 1, "radius": "10", "color": colors[%s], "tag": "%s" }' %(choice([0,1,2]),experiment[0]["tag"]))
+
+# Generate index page
+index_template = "".join(open("index.html","rb").readlines())
+index_template = index_template.replace("[SUB_NODES_SUB]",",".join(nodes))
+index_template = index_template.replace("[SUB_TOTAL_SUB]",str(len(nodes)))
+filey = open(output_index,"wb")
+filey.writelines(index_template)
+filey.close()
 
 # Clear old experiments
 experiment_dir = os.path.abspath("../static/experiments/")
 shutil.rmtree(experiment_dir)
-os.mkdir(experiment_dir)
 
 # Copy updated valid experiments into our experiment directory
 battery_dest = os.path.abspath("..")
@@ -53,11 +71,42 @@ valid_experiments = ["%s/%s" %(experiment_repo,x[0]["tag"]) for x in experiments
 template_file = os.path.abspath("load_experiment.js")
 template_experiments(battery_dest,battery_repo,valid_experiments,template_file=template_file)
 
+# For each experiment, we will generate a demo page
+for experiment in experiments:
+    js = ""
+    css = ""
+    demo_page = os.path.abspath("../%s.html" %experiment[0]["tag"])
+    # Get the scripts, add to variables depending on extension    
+    scripts = experiment[0]["run"]
+    for script in scripts:
+        ext = script.split(".")[-1]
+        # Do we have a relative experiment path?
+        if len(script.split("/")) == 1:
+            if ext == "js":
+                js = "%s\n<script src='static/experiments/%s/%s'></script>" %(js,experiment[0]["tag"],script)
+            elif ext == "css":
+                css = "%s\n<link rel='stylesheet prefetch' href='static/experiments/%s/%s'>" %(css,experiment[0]["tag"],script)
+        # Do we have a battery relative path?
+        else:    
+            if ext == "js":
+                js = "%s\n<script src='%s'></script>" %(js,script)
+            elif ext == "css":
+                css = "%s\n<link rel='stylesheet prefetch' href='%s'>" %(css,script)
+
+    # Write to output file
+    exp_template = "".join(open("experiment.html","rb").readlines())
+    exp_template = sub_template(exp_template,"[SUB_JSSCRIPTS_SUB]",js)
+    exp_template = sub_template(exp_template,"[SUB_STYLE_SUB]",css)
+    exp_template = sub_template(exp_template,"[SUB_TAG_SUB]",experiment[0]["tag"])
+    filey = open(demo_page,"wb")
+    filey.writelines(exp_template)
+    filey.close()
+
 # First prepare rendered table
-table = '<table border="1"  id="chart" class="dataframe table table-striped table-bordered"><thead><tr style="text-align: right;">\n<tr>\n'
+table = '<table id="fresh-table" class="table">\n<thead>\n'
 for field in fields:
-    table = "%s<th>%s</th>" %(table,field)
-table = "%s</tr>\n</thead>\n<tbody>\n" %(table)
+    table = '%s<th data-field="%s" data-sortable="true">%s</th>' %(table,field,field)
+table = '%s\n</thead>\n<tbody>\n' %(table)
 
 for row in valid.iterrows():
     table = "%s<tr>\n" %(table)
@@ -67,23 +116,11 @@ for row in valid.iterrows():
 
 table = "%s</tbody></table>\n" %(table)
 
-# Write the new index
-index_template = "".join(open("index.html","rb").readlines())
-index_template = index_template.replace("[[SUB_TABLE_SUB]]",table)
-filey = open(output_index,"wb")
-filey.writelines(index_template)
-filey.close()
-
-# Write experiment hub
-hub_template = "".join(open("exp_hub.html","rb").readlines())
-timingjs = get_timing_js(valid_experiments)
-loadjs = get_load_js(valid_experiments) 
-concatjs = get_concat_js(valid_experiments) 
-hub_template = sub_template(hub_template,"[SUB_EXPERIMENTTIMES_SUB]",str(timingjs))
-hub_template = sub_template(hub_template,"[SUB_EXPERIMENTCONCAT_SUB]",concatjs)
-hub_template = sub_template(hub_template,"[SUB_EXPERIMENTLOAD_SUB]",loadjs)
-filey = open(output_hub,"wb")
-filey.writelines(hub_template)
+# Write the new table
+table_template = "".join(open("table.html","rb").readlines())
+table_template = table_template.replace("[[SUB_TABLE_SUB]]",table)
+filey = open(output_table,"wb")
+filey.writelines(table_template)
 filey.close()
 
 # Function to save pretty json file
