dd43f15e7752aa529fabc3c6de518703cd1c1910
vsoch <vsochat@stanford.edu>
2016-01-18 18:30:58 -0800

    adding remainder of experiment files

---
+++ static/experiments/random_number_generation/experiment.js
@@ -0,0 +1,143 @@
+/* ************************************ */
+/* Define helper functions */
+/* ************************************ */
+
+var randomDraw = function(lst) {
+    var index = Math.floor(Math.random()*(lst.length))
+    return lst[index]
+}
+
+
+/* ************************************ */
+/* Define experimental variables */
+/* ************************************ */
+var grid = 
+    '<div class = numbox>' +
+    '<button id = button_1 class = "square num-button"><div class = numbers>1</div></button>' +
+    '<button id = button_2 class = "square num-button"><div class = numbers>2</div></button>' +
+    '<button id = button_3 class = "square num-button"><div class = numbers>3</div></button>' +
+    '<button id = button_4 class = "square num-button"><div class = numbers>4</div></button>' +
+    '<button id = button_5 class = "square num-button"><div class = numbers>5</div></button>' +
+    '<button id = button_6 class = "square num-button"><div class = numbers>6</div></button>' +
+    '<button id = button_7 class = "square num-button"><div class = numbers>7</div></button>' +
+    '<button id = button_8 class = "square num-button"><div class = numbers>8</div></button>' +
+    '<button id = button_9 class = "square num-button"><div class = numbers>9</div></button>' +
+    '</div>'
+
+var empty_grid = 
+    '<div class = numbox><div class = "blank-square num-button"><div class = content><div class = numbers ></div></div></div>' +
+    '<div class = "blank-square num-button"><div class = content><div class = numbers ></div></div></div>' +
+    '<div class = "blank-square num-button"><div class = content><div class = numbers ></div></div></div>' +
+    '<div class = "blank-square num-button"><div class = content><div class = numbers ></div></div></div>' +
+    '<div class = "blank-square num-button"><div class = content><div class = numbers ></div></div></div>' +
+    '<div class = "blank-square num-button"><div class = content><div class = numbers ></div></div></div>' +
+    '<div class = "blank-square num-button"><div class = content><div class = numbers ></div></div></div>' +
+    '<div class = "blank-square num-button"><div class = content><div class = numbers ></div></div></div>' +
+    '<div class = "blank-square num-button"><div class = content><div class = numbers ></div></div></div></div>'
+
+
+
+
+
+
+
+
+var num_trials = 162 
+practice_stims = []
+test_stims = []
+for (var i = 0; i < 10; i++) {
+  practice_stims.push(grid)
+}
+for (var i = 0; i < num_trials; i++) {
+  test_stims.push(grid)
+}
+/* ************************************ */
+/* Set up jsPsych blocks */
+/* ************************************ */
+/* define static blocks */
+
+var welcome_block = {
+  
+  type: 'text',
+  text: '<div class = centerbox><p class = block-text>Welcome to the random number generation task experiment. Press <strong>enter</strong> to begin.</p></div>',
+  cont_key: [13],
+  timing_post_trial: 0
+};
+
+var instructions_block = {
+  type: 'poldrack-instructions',
+  pages: [
+    '<div class = centerbox><p class = block-text>In this task, your job is to generate a random sequence of digits. You will do this by clicking on a virtual numpad using your mouse. Once you click, the number will temporarily turn red. You have less than a second to respond on each trial so it is important to respond quickly!</p><p class = block-text>.After the trial ends the numbers will dissapear for a moment. When they appear again the next trial has begun and you should respond as quickly as possible.</p><p class = block-text>Your goal is to choose each number completely randomly, as if you were picking a number from a hat with 9 slips of paper, reading it, and placing it back before picking another number.</p></div>',
+  ],
+  allow_keys: false,
+  show_clickable_nav: true,
+  //timing_post_trial: 1000
+};
+
+var end_block = {
+  type: 'text',
+  text: '<div class = centerbox><p class = center-block-text>Thanks for completing this task!</p><p class = center-block-text>Press <strong>enter</strong> to begin.</p></div>',
+  cont_key: [13],
+  timing_post_trial: 0
+};
+
+var start_practice_block = {
+  type: 'text',
+  text: '<div class = centerbox><p class = center-block-text>Starting a practice block.</p><p class = center-block-text>Press <strong>enter</strong> to begin.</p></div>',
+  cont_key: [13],
+  timing_post_trial: 1000
+};
+
+var start_test_block = {
+  type: 'text',
+  text: '<div class = centerbox><p class = center-block-text>Starting a test block.</p><p class = center-block-text>Press <strong>enter</strong> to begin.</p></div>',
+  cont_key: [13],
+  timing_post_trial: 1000
+};
+
+var  wait_block = {
+  type: 'poldrack-single-stim',
+  stimuli: [empty_grid],
+  choices: 'none',
+  is_html: true,
+  data: {exp_id: "random_number_generation", trial_id: "wait"},
+  timing_stim: 200,
+  timing_response: 200,
+  response_ends_trial: false,
+  timing_post_trial: 0
+};
+
+//Set up experiment
+var random_number_generation_experiment = []
+random_number_generation_experiment.push(welcome_block);
+random_number_generation_experiment.push(instructions_block);
+random_number_generation_experiment.push(start_practice_block);
+for (var i = 0; i <practice_stims.length; i++) {
+  var  practice_block = {
+    type: 'single-stim-button',
+    stimuli: [practice_stims[i]],
+    button_class: 'num-button',
+    data: {exp_id: "random_number_generation", trial_id: "practice"},
+    timing_response: 800,
+    response_ends_trial: false,
+    timing_post_trial: 0
+  };
+  random_number_generation_experiment.push(practice_block)
+  random_number_generation_experiment.push(wait_block)
+}
+random_number_generation_experiment.push(start_test_block);
+//Loop should be changed to go until test_stims.length later
+for (var i = 0; i <practice_stims.length; i++) {
+  var  test_block = {
+    type: 'single-stim-button',
+    stimuli: [test_stims[i]],
+    button_class: 'num-button',
+    data: {exp_id: "rng", trial_id: "test"},
+    timing_response: 800,
+    response_ends_trial: false,
+    timing_post_trial: 0
+  };
+  random_number_generation_experiment.push(test_block)
+  random_number_generation_experiment.push(wait_block)
+}
+random_number_generation_experiment.push(end_block)
---
+++ static/experiments/simple_reaction_time/experiment.js
@@ -0,0 +1,110 @@
+
+/* ************************************ */
+/* Define helper functions */
+/* ************************************ */
+var post_trial_gap = function() {
+  gap = Math.floor( Math.random() * 2000 ) + 1000
+  return gap;
+}
+
+/* Append gap and current trial to data and then recalculate for next trial*/
+var appendData = function() {
+	jsPsych.data.addDataToLastTrial({ITI: gap, trial_num: current_trial})
+	current_trial = current_trial + 1
+}
+
+/* ************************************ */
+/* Define experimental variables */
+/* ************************************ */
+var practice_len = 5
+var experiment_len = 50
+var gap = 0
+var current_trial = 0
+stim = '<div class = shapebox><div id = cross></div></div>'
+
+
+
+/* ************************************ */
+/* Set up jsPsych blocks */
+/* ************************************ */
+/* define static blocks */
+var welcome_block = {
+  type: 'text',
+  text: '<div class = centerbox><p class = block-text>Welcome to the simple RT experiment. Press <strong>enter</strong> to begin.</p></div>',
+  cont_key: [13],
+  timing_post_trial: 0
+};
+
+var end_block = {
+  type: 'text',
+  text: '<div class = centerbox><p class = center-block-text>Thanks for completing this task!</p><p class = center-block-text>Press <strong>enter</strong> to continue.</p></div>',
+  cont_key: [13],
+  timing_post_trial: 0
+};
+
+var instructions_block = {
+  type: 'poldrack-instructions',
+  pages: ['<div class = centerbox><p class = block-text>In this experiment, we are testing how fast you can respond. On each trial press the spacebar as quickly as possible <strong>after</strong> you see the large "X".</p></div>'],
+  allow_keys: false,
+  show_clickable_nav: true,
+  timing_post_trial: 1000
+};
+
+var start_practice_block = {
+  type: 'text',
+  text: '<div class = centerbox><p class = center-block-text>We will start 5 practice trials. Press <strong>enter</strong> to begin.</p></div>',
+  cont_key: [13],
+  timing_post_trial: 1000
+};
+
+var start_test_block = {
+  type: 'text',
+  text: '<div class = centerbox><p class = block-text>We will now start the test. Respond to the "X" as quickly as possible by pressing the spacebar. Press <strong>enter</strong> to begin.</p></div>',
+  cont_key: [13],
+  timing_post_trial: 1000
+};
+
+var reset_block = {
+	type: 'call-function',
+	func: function() {
+		current_trial = 0
+	},
+	timing_post_trial: 0
+}
+
+/* define practice block */
+var practice_block = {
+  type: 'poldrack-single-stim',
+  stimulus: stim,
+  is_html: true,
+  data: {exp_id: "simple_reaction_time", trial_id: "practice"},
+  choices: [32],
+  timing_post_trial: post_trial_gap,
+  on_finish: appendData
+};
+
+/* define test block */
+var test_block = {
+  type: 'poldrack-single-stim',
+  stimulus: stim,
+  is_html: true,
+  data: {exp_id: "simple_reaction_time", trial_id: "test"},
+  choices: [32],
+  timing_post_trial: post_trial_gap,
+  on_finish: appendData
+};
+
+/* create experiment definition array */
+var simple_reaction_time_experiment = [];
+simple_reaction_time_experiment.push(welcome_block);
+simple_reaction_time_experiment.push(instructions_block);
+simple_reaction_time_experiment.push(start_practice_block);
+for (var i = 0; i < practice_len; i++) {
+	simple_reaction_time_experiment.push(practice_block);
+}
+simple_reaction_time_experiment.push(reset_block)
+simple_reaction_time_experiment.push(start_test_block);
+for (var i = 0; i < experiment_len; i++) {
+	simple_reaction_time_experiment.push(test_block);
+}
+simple_reaction_time_experiment.push(end_block);
